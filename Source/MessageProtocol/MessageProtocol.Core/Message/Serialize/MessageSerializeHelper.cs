2abfff7bef7ab956326bf62b78afd26f663fbffc
bigsungg <bigsungg@gmail.com>
2025-12-27 16:57:40 +0900

    프로젝트 이름 변경

---
+++ Source/MessageProtocol/MessageProtocol.Core/Message/Serialize/MessageSerializeHelper.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DS.Message.Serialize
+{
+    public class MessageSerializeHelper
+    {
+        public MessageSerializeHelper(Type messageType)
+        {
+            
+        }
+    }
+}
---
+++ Source/MessageProtocol/MessageProtocol.Core/Message/Serialize/MessageSerializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DS.Message;
+
+namespace DS.Message.Serialize
+{
+    public class MessageSerializer
+    {
+        public static MessageSerializer Instance => _instance.Value;
+        static Lazy<MessageSerializer> _instance = new Lazy<MessageSerializer>(() => new MessageSerializer());
+
+        Dictionary<Type, MessageSerializeHelper> _serializeHelper = new Dictionary<Type, MessageSerializeHelper>();
+
+        public MessageSerializer()
+        {
+            // 현재 어셈블리에서 MessageInfo 어트리뷰트를 가진 모든 클래스를 찾은 후 SerializeHelper에 등록            
+            var assembly = Assembly.GetExecutingAssembly();
+            var types = assembly.GetTypes()
+                .Where(type => type.GetCustomAttribute<MessageInfo>() != null);
+
+            foreach (var type in types)
+            {
+                var attribute = type.GetCustomAttribute<MessageInfo>();
+                if (attribute != null)
+                {
+                    _serializeHelper[type] = new MessageSerializeHelper(type);
+                }
+            }
+        }        
+
+        //public byte[] Serialize(object message)
+        //{
+            
+        //}
+    }
+}
